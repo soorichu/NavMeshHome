41a04d1cb538ca3383ec3031b0cb5fe35c6d39d2
soorichu <soorichu@gmail.com>
2021-10-23 18:46:26 +0900

    complete

--- Assets/Scripts/LetsMove.cs
+++ Assets/Scripts/LetsMove.cs
@@ -6,18 +6,26 @@ public class LetsMove : MonoBehaviour
 {
     NavMeshAgent agent;
     public Transform target;
+    public Transform older;
+    bool seeked;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        seeked = false;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !seeked)
         {
             agent.SetDestination(target.position);
+        }
+        if(Vector3.Distance(transform.position, target.position) < 2)
+        {
+            seeked = true;
+            agent.SetDestination(older.position);
         }
     }
 }
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public GameObject agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Vector3.Distance(transform.position, agent.transform.position)<2)
+        {
+            transform.position = agent.transform.position + new Vector3(0, 1, 0);
+        }
+  //      Debug.Log(Vector3.Distance(transform.position, agent.transform.position));
+    }
+}
